de8ec4a965367b536c3bc551244a1e450e76d943
Alexei-P <alexei.parkhomchyk@gmail.com>
2017-02-12 00:02:06 +0300

    tasks 13-17

--- src/by/epam/tr/lesson2/Main.java
+++ src/by/epam/tr/lesson2/Main.java
@@ -9,6 +9,10 @@ public class Main {
 		//calcTask6();
 		//calcTask7(5.0, 5.2, 8);
 		//calcTask8(5.0, 5.2, 8);
+		//calcTask14(-4,3,5);
+		//calcTask15(2, 4, 0.1);
+		//calcTask16(6);
+		
 	}
 	
 	public static void calcTask6(){
@@ -90,8 +94,72 @@ public class Main {
 					
 	}
 	
+	public static void calcTask13a(double a, double b, double c){
+		double numerator = b + (Math.sqrt(b*b + 4*a*c));
+		double denominator = 2*a;
+		double result = numerator/denominator - Math.pow(a, 3)*c + b;
+		
+		System.out.println("Expression result = "+result);
+	}
 	
+	public static void calcTask13b(double a, double b, double c, double d){
+		double result = (a/c*b/d) - ((a*b-c) / c*d);
+		System.out.println(result);
+	}
 	
-	
+	public static void calcTask14(double a, double b, double c){
+		if (a>=0) {
+			a *= a;
+			}
+		else {
+			a = a*a*a*a;
+		}
+		if (b>=0) {
+			b *= b;
+			}
+		else {
+			b = b*b*b*b;
+		}
+		if (c>=0) {
+			c *= c;
+			}
+		else {
+			c = c*c*c*c;
+		}
+		
+		System.out.println("a = "+a+""
+				+ " b = "+b
+				+ " c = "+c
+				);
+	}
 
+	public static void calcTask15(double a, double b, double h){
+		double temp=a;
+		double fx=0;
+		System.out.println("1st table x 2nd F(x)");
+		while(temp<=b){
+			fx = 2*Math.tan(temp/2)+1;
+			System.out.println("x = "+ temp+ "F(x) = "+fx);
+			temp+=h;
+		}
+	}
+
+	public static void calcTask16(double R){
+		double circleSquare = Math.PI*R*R;
+		double circleLength = 2*Math.PI*R;
+		System.out.println("Circle square = "+circleSquare+" Circle length = "+circleLength);
+	}
+
+	public static void calcTask17(double x){
+		if (x<=-3){
+			System.out.println("F(x) = "+9);
+		}
+		else if (x>3){
+			double result = 1 / (x*x + 1);
+			System.out.println("F(x) = "+result);
+		}
+		else{
+			System.out.println("Function domain is not defined");
+		}
+	}
 }
